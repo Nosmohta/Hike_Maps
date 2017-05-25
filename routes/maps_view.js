6a923efd9ed87e62ca232aa257b14aa43b253cde
Andrew Thomson <Thomson.w.andrew@gmail.com>
2017-05-25 19:28:22 +0000

    server api resourses connected to client side

--- routes/maps_view.js
+++ routes/maps_view.js
@@ -1,15 +1,19 @@
-const express       = require("express");
-const router        = express.Router();
+"use strict";
 
-
-
+const express = require('express');
+const router  = express.Router();
 
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
-       res.render("maps_index");
+    knex
+      .select("*")
+      .from("users")
+      .then((results) => {
+
+        res.json(results);
+    });
   });
 
   return router;
 }
-
