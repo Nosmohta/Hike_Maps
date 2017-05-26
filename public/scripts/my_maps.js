bb5fb8f6860602884939ec08d9426675761dec63
Dimas Andiputra <andiputra.dimas@hotmail.com>
2017-05-26 18:24:00 +0000

    middle of working on map view

--- public/scripts/my_maps.js
+++ public/scripts/my_maps.js
@@ -5,5 +5,17 @@ $(() => {
   // }).done((data) => {
 
   //   })
+     $.ajax({
+      method: "GET",
+      url: "/api/maps"
+     }).done((map) => {
+        for (hikes of map) {
+          console.log(hikes.title)
+          let hike = $("<a>").text(hikes.title).attr("href", `/maps/${hikes.id}`);
+          let editbutton= $("<button>").attr("type", "button").addClass("btn btn-primary").text("edit");
+          let li= $("<li>").append(hike).append(editbutton).addClass("h5");
+          $(".myhikes").append(li);
+        }
+     });
 
 });
