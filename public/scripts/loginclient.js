b5b014ae525f9c0bd23d9e71fef721e4da41b940
Andrew Thomson <Thomson.w.andrew@gmail.com>
2017-05-26 00:46:31 +0000

    login and logout fully functional. username stored in cookie

--- public/scripts/loginclient.js
+++ public/scripts/loginclient.js
@@ -7,19 +7,10 @@ $(() => {
     url: "/api/users"
   }).done((users) => {
     for(user of users) {
-      console.log(user.name);
       let input = $("<input  type='submit' >").val(user.name)
-      let form = $("<form method='GET' path='/login'>").attr("path", "/login/" + user.name).append(input);
+      let form = $("<form method='POST' action='/login'>").attr( "action", "/users/" + user.name).append(input);
       $("#autologin").append(form);
 
-      // // add event listner to the buttons
-      // $([val = user.name]).on("click", () => {
-      //   preventDefault();
-      //   //ajax req to login
-      //   //
-
-
-      // })
 
     }
 
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const ENV         = process.env.ENV || "development";
 const express     = require("express");
 const bodyParser  = require("body-parser");
 const sass        = require("node-sass-middleware");
+const session     = require('express-session')
 const app         = express();
 
 const knexConfig  = require("./knexfile");
@@ -46,6 +47,16 @@ app.use("/api/maps/mapid", mapsviewRoutes(knex));
 app.use("/api/maps", mapsRoutes(knex));
 app.use("/api/user/:userid/:mapid", editviewRoutes(knex));
 
+// session cookie config
+app.use(session( {
+    name: 'session',
+    secret: "bigsecret",
+    resave: false,
+    saveUninitialized: true,
+    maxAge: 24 * 60 * 60 * 1000
+  }
+));
+
 
 // Home page
 app.get("/", (req, res) => {
@@ -68,16 +79,26 @@ app.get("/users",  (req, res) => {
 });
 
 // Create/Edit Map Page
-app.get("/users/:userid/:mapid",  (req, res) => {
+app.get("/users/:userid",  (req, res) => {
   res.render("edit_view");
 });
 
-
 // login
-app.get("/login/:name", (req, res) => {
-  console.log(req.params.name);
-  res.redirect("/maps")
-})
+app.post("/users/:userid", (req, res) => {
+  console.log("user name:", req.params.userid);
+  let userid = req.params.userid;
+  req.session.userID = userid;
+  res.redirect("/users/" + userid)
+});
+
+// logout
+app.post("/logout",(req, res) => {
+  req.session.destroy(function(err) {
+    console.log("logout. Check cookies")
+    res.redirect("/maps");
+  })
+});
+
 
 
 app.listen(PORT, () => {
